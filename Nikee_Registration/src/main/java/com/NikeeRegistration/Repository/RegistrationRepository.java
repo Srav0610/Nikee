8a878b44a02d88fc0eb54d32fb805ad8db4aa2e1
sravanthi <sravanthimanchikatla06@gmail.com>
2025-01-20 13:05:56 +0530

    updated

--- Nikee_Registration/src/main/java/com/NikeeRegistration/Repository/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.NikeeRegistration.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.NikeeRegistration.Entity.Registration;
-
-public interface RegistrationRepository extends JpaRepository<Registration, String>{
-
-}
